ed271906630c41891bb3566770c2ae5ede173d2c
Osama Hussein <49457438+OsamaHu1277@users.noreply.github.com>
2021-01-07 14:35:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,49 +1,42 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "/";
+var prefix = "!";
 const db = require("quick.db");
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag} !`);
-          client.user.setActivity("YOUR DM",{type: 'WATCHING'});
-  
-  });
- 
-const serverStats = {
-    guildID: '432217617714118656', //SERVER ID HERE
-    ticketCategoryID: '602577299086245918'//TICKET CATEGORY ID HERE
- 
+});
+
+const RequireIDs = {
+    GuildId: 'Server ID', //Your Server ID
+    TicketCategoryId: 'Category ID', //Tickets Gategory
+    LogChannel: 'Log Channel ID'
 }
- 
- 
-//It's not a command, just send a DM for bot and...
- 
+
+//When Someone Send Message to the bot , will create a channel for him on th server
 client.on('message', async message => {
     if (message.author.bot) return;
     if (message.channel.type !== 'text') {
         let active = await db.fetch(`support_${message.author.id}`);
-        let guild = client.guilds.get(serverStats.guildID);
+        let guild = client.guilds.cache.get(RequireIDs.GuildId);
         let channel, found = true;
         try {
-            if (active) client.channels.get(active.channelID)
-                .guild;
+            if (active) client.channels.cache.get(active.channelID).guild;
         } catch (e) {
             found = false;
         }
         if (!active || !found) {
             active = {};
             channel = await guild.createChannel(`${message.author.username}-${message.author.discriminator}`)
-            channel.setParent(serverStats.ticketCategoryID)
-            channel.setTopic(`/complete to close the ticket | Support for ${message.author.tag} | ID: ${message.author.id}`)
- 
-            channel.overwritePermissions("432217617714118656", { //Role id (when someone join my server get this role with id <<, i dont know how to change it for @everyone. This will prevent users to see the channel, only admins will see!
+            channel.setParent(RequireIDs.TicketCategoryId)
+            channel.setTopic(`${prefix}complete to close the ticket | Support for ${message.author.tag} | ID: ${message.author.id}`)
+
+            channel.overwritePermissions(`${guild.id}`, { //Hide Channel for @everyone
                 VIEW_CHANNEL: false,
-                SEND_MESSAGES: false,
-                ADD_REACTIONS: false
-            });//Toxic Codes
- 
- 
- 
+            });
+
             let author = message.author;
+
+
             const newChannel = new Discord.RichEmbed()
                 .setColor('RANDOM')
                 .setAuthor(author.tag, author.avatarURL)
@@ -51,19 +44,22 @@ client.on('message', async message => {
                 .addField('User', author)
                 .addField('ID', author.id)
             await channel.send(newChannel);
+
+
             const newTicket = new Discord.RichEmbed()
                 .setColor('RANDOM')
                 .setAuthor(`Hello, ${author.username}`, author.avatarURL)
-                .setFooter('Support Ticket Created!  تم فتح تذكرة دعم')
+                .setFooter('Support Ticket Created!')
             await author.send(newTicket);
             active.channelID = channel.id;
             active.targetID = author.id;
         }
-        channel = client.channels.get(active.channelID);//Toxic Codes
+
+        channel = client.channels.cache.get(active.channelID);
         const dm = new Discord.RichEmbed()
             .setColor('RANDOM')
             .setAuthor(`Thank you, ${message.author.username}`, message.author.avatarURL)
-            .setFooter(`Your message has been sent - A staff member will be in contact soon.تم إرسال رسالتك - سوف يرد عليك أحد المسئولين قريبًا`)//Toxic Codes
+            .setFooter(`Your message has been sent - A staff member will be in contact soon.`)
         await message.author.send(dm);
         if (message.content.startsWith(prefix + 'complete')) return;
         const embed5 = new Discord.RichEmbed()
@@ -73,20 +69,20 @@ client.on('message', async message => {
             .setFooter(`Message Received - ${message.author.tag}`)
         await channel.send(embed5);
         db.set(`support_${message.author.id}`, active);
-        db.set(`supportChannel_${channel.id}`, message.author.id); //Toxic Codes
+        db.set(`supportChannel_${channel.id}`, message.author.id);
         return;
     }
-    let support = await db.fetch(`supportChannel_${message.channel.id}`); //Toxic Codes
+    let support = await db.fetch(`supportChannel_${message.channel.id}`);
     if (support) {
         support = await db.fetch(`support_${support}`);
-        let supportUser = client.users.get(support.targetID);
+        let supportUser = client.users.cache.get(support.targetID);
         if (!supportUser) return message.channel.delete();
-        if (message.content.toLowerCase() === '/complete') { 
+        if (message.content.toLowerCase() === '/complete') {
             const complete = new Discord.RichEmbed()
                 .setColor('RANDOM')
                 .setAuthor(`Hey, ${supportUser.tag}`, supportUser.avatarURL)
-                .setFooter('Ticket Closed -- تم اغلاق التذكرة')
-                .setDescription('**Your ticket has been marked as complete. If you wish to reopen it, or create a new one, please send a message to the bot. لقد تم إغلاق المحادئة اذا كنت ترغب في استكمال المحادثة لا تتردد في إرسال رسالة اخري للبوت**')
+                .setFooter('Ticket Closed')
+                .setDescription('Your ticket has been marked as complete. If you wish to reopen it, or create a new one, please send a message to the bot.')
             supportUser.send(complete);
             message.channel.delete();
             db.delete(`support_${support.targetID}`);
@@ -95,22 +91,25 @@ client.on('message', async message => {
                 .addField('Support User', `${supportUser.tag}`)
                 .addField('Closer', message.author.tag)
                 .setColor('RANDOM')
-            const staffChannel = client.channels.get('602580415361843201'); //Create a log channel and put id here
-            staffChannel.send(inEmbed); 
+            const staffChannel = client.channels.cache.get(RequireIDs.LogChannel); //Create a log channel and put id here
+            if (staffChannel) {
+                staffChannel.send(inEmbed);
+            }
         }
-        const embed4 = new Discord.RichEmbed() 
+        const embed4 = new Discord.RichEmbed()
             .setColor('RANDOM')
             .setAuthor(message.author.tag, message.author.avatarURL)
-            .setFooter(`Message Received - تم ارسال الرسالة`)
+            .setFooter(`Message Received`)
             .setDescription(message.content)
-        client.users.get(support.targetID)
+        client.users.cache.get(support.targetID)
             .send(embed4);
         message.delete({
             timeout: 10000
-        }); 
-        embed4.setFooter(`Message Sent -- ${supportUser.tag}`) 
+        });
+        embed4.setFooter(`Message Sent -- ${supportUser.tag}`)
             .setDescription(message.content);
-        return message.channel.send(embed4); 
+        return message.channel.send(embed4);
     }
-}); 
+});
+
 client.login(process.env.BOT_TOKEN);
